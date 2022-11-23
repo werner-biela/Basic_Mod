7d864c7f3451327ba16ce5f59f755a7481011107
Werner Biela <werner.biela@hotmail.com>
2022-11-23 14:06:59 +0000

    Added code pretending to Basic-Mod2 challenge.
    Altered Main.java to be more modular. Altered NumberMapping.java to include a 2nd set of numbers to characters map.

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,36 @@
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        String file = "others//message.txt";
-        ArrayList<Integer> numbers = new ArrayList<>();
-        NumberMapping convert = new NumberMapping();
-        
-        /* 
-        * Read the data from the text file and parse it into an array of integers
-        * to be processed. 
-        */
+        String file1 = "others//message1.txt";
+        String file2 = "others//message2.txt";
+        ArrayList<Integer> numberSet1;
+        ArrayList<Integer> numberSet2;
+
+        numberSet1 = ReadFileToArray(file1);
+        numberSet2 = ReadFileToArray(file2);
+
+        FindModResults(new NumberMapping(false), FindModOf(numberSet1),"basic-mod1");
+        FindModResults(new NumberMapping(true), FindModInverseOf(numberSet2),"basic-mod2");
+        System.out.println();
+    }
+    private static ArrayList<Integer> ReadFileToArray(String file){
+        /*
+         * Read the data from the text file and parse it into an array of integers
+         * to be processed.
+         */
+        ArrayList<Integer> collectedNumbers = new ArrayList<>();
         try{
             File myObj = new File(file);
             Scanner myReader = new Scanner(myObj);
             while(myReader.hasNextLine()){
                 String[] data = myReader.nextLine().split(" ");
                 for (String datum : data) {
-                    numbers.add(Integer.parseInt(datum));
+                    collectedNumbers.add(Integer.parseInt(datum));
                 }
             }
             myReader.close();
@@ -28,16 +39,17 @@ public class Main {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+        return collectedNumbers;
+    }
 
+    private static void FindModResults(NumberMapping convert, int[] numbers, String PicoCTF_challenge){
         /*
-        * Find out the mod37 of each number in the Integer ArrayList,
-        * and map out the results to their given character to be
-        * displayed on console.
-        */
-        int[] modResults = FindModOf(numbers);
-        System.out.print("Flag: picoCTF{");
-        for (int i = 0; i < numbers.size(); i++){
-            System.out.print(convert.MapTo(modResults[i]));
+         * Maps out the results to their given character to be
+         * displayed on console.
+         */
+        System.out.print("\n" + PicoCTF_challenge.toUpperCase() + " Flag: picoCTF{");
+        for (int number : numbers) {
+            System.out.print(convert.MapTo(number));
         }
         System.out.print("}");
     }
@@ -49,5 +61,23 @@ public class Main {
             modList[i] = array.get(i) % 37;
         }
         return modList;
+    }
+
+    private static int[] FindModInverseOf(ArrayList<Integer> array){
+        int[] modInverseList = new int[array.size()];
+        for (int i = 0; i < array.size(); i++){
+            modInverseList[i] = calModInv(array.get(i), 41 );
+        }
+        return modInverseList;
+    }
+
+    private static int calModInv(int a, int b){
+        a = a % b;
+        for (int x = 1; x < b; x++){
+            if((a*x)%b == 1){
+                return x;
+            }
+        }
+        return 1;
     }
 }
--- src/NumberMapping.java
+++ src/NumberMapping.java
@@ -4,11 +4,20 @@ public class NumberMapping {
 
     private static final HashMap<Integer, Character> numberMapping = new HashMap<>();
 
-    public NumberMapping() {
-        int[] numbers = new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36};
-        char[] chars = new char[]{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','_'};
-        for(int i = 0; i < numbers.length; i++){
-            numberMapping.put(numbers[i], chars[i]);
+    public NumberMapping(boolean altSet) {
+        if (altSet){
+            int[] numbers = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37};
+            char[] chars = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','_'};
+            for(int i = 0; i < numbers.length; i++){
+                numberMapping.put(numbers[i], chars[i]);
+            }
+        }
+        else {
+            int[] numbers = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36};
+            char[] chars = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '_'};
+            for (int i = 0; i < numbers.length; i++) {
+                numberMapping.put(numbers[i], chars[i]);
+            }
         }
     }
 
