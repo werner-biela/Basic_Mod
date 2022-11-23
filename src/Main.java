3928c30ae4b98f56e1a924b1aa09ad188b0637c6
Werner Biela <werner.biela@hotmail.com>
2022-11-23 22:58:52 +0000

    Minor changes to code
    Added comments that were missing and relocated the fields outside of the Main().

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,26 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+    // << Fields >>
+    private static final String file1 = "others//message1.txt";
+    private static final String file2 = "others//message2.txt";
+
     public static void main(String[] args) {
 
-        String file1 = "others//message1.txt";
-        String file2 = "others//message2.txt";
-        ArrayList<Integer> numberSet1;
-        ArrayList<Integer> numberSet2;
+        ArrayList<Integer> numberSet1 = ReadFileToArray(file1);
+        ArrayList<Integer> numberSet2 = ReadFileToArray(file2);
 
-        numberSet1 = ReadFileToArray(file1);
-        numberSet2 = ReadFileToArray(file2);
-
-        FindModResults(new NumberMapping(false), FindModOf(numberSet1),"basic-mod1");
-        FindModResults(new NumberMapping(true), FindModInverseOf(numberSet2),"basic-mod2");
+        DisplayMapResults(new NumberMapping(false), FindModOf(numberSet1),"basic-mod1");
+        DisplayMapResults(new NumberMapping(true), FindModInverseOf(numberSet2),"basic-mod2");
         System.out.println();
     }
+
+    // << Methods >>
+
+    //Reads the data from the text file and parse it into an array of integers to be processed.
     private static ArrayList<Integer> ReadFileToArray(String file){
-        /*
-         * Read the data from the text file and parse it into an array of integers
-         * to be processed.
-         */
+
         ArrayList<Integer> collectedNumbers = new ArrayList<>();
         try{
             File myObj = new File(file);
@@ -42,11 +43,8 @@ public class Main {
         return collectedNumbers;
     }
 
-    private static void FindModResults(NumberMapping convert, int[] numbers, String PicoCTF_challenge){
-        /*
-         * Maps out the results to their given character to be
-         * displayed on console.
-         */
+    // Maps out the results to their given character to be displayed on console.
+    private static void DisplayMapResults(NumberMapping convert, int[] numbers, String PicoCTF_challenge){
         System.out.print("\n" + PicoCTF_challenge.toUpperCase() + " Flag: picoCTF{");
         for (int number : numbers) {
             System.out.print(convert.MapTo(number));
@@ -63,20 +61,23 @@ public class Main {
         return modList;
     }
 
+    /*
+     * Method that finds out the inverse mod41 of the numbers
+     * in a given Integer ArrayList using calculateModInverse().
+     */
     private static int[] FindModInverseOf(ArrayList<Integer> array){
         int[] modInverseList = new int[array.size()];
         for (int i = 0; i < array.size(); i++){
-            modInverseList[i] = calModInv(array.get(i), 41 );
+            modInverseList[i] = calculateModInverse(array.get(i), 41 );
         }
         return modInverseList;
     }
 
-    private static int calModInv(int a, int b){
+    @SuppressWarnings("SameParameterValue")
+    private static int calculateModInverse(int a, int b){
         a = a % b;
         for (int x = 1; x < b; x++){
-            if((a*x)%b == 1){
-                return x;
-            }
+            if((a * x) % b == 1) return x;
         }
         return 1;
     }
